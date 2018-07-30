79aa3fe01b9be41c13b0dd46d4164afcd36ee58b
Rob Zienert <rzienert@netflix.com>
2018-07-30 11:59:03 -0700

    some more error handling

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ This, combined with my [dotfiles](https://github.com/robzienert/dotfiles) and a 
 
 You'll need homebrew, I've only written this for OSX so far, but can easily be linux'd as well.
 
-1. Clone into `~/.spinnaker-dotfiles`
+1. Clone into `~/.spinnaker-setup`
 1. `$ ./install.sh <github_username>`
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 if [ "$#" -eq 0 ]; then
   echo "usage: install.sh <github_username>"
   echo
@@ -23,13 +23,33 @@ source /tmp/spinnaker-setup.sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 bork do ok symlink $HOME/.spinnaker-env.sh $DIR/files/env.sh
+if did_error; then
+  echo "Failed symlinking environment"
+  exit 1
+fi
 source $HOME/.spinnaker-env.sh
 
 bork satisfy satisfy/osx.sh
+if did_error; then
+  echo "Failed satisfying OSX requirements"
+  exit 1
+fi
+
 bork satisfy satisfy/repos.sh
+if did_error; then
+  echo "Failed satisfying repo setup"
+  exit 1
+fi
 
 bork do ok directory $HOME/.spinnaker
+if did_error; then
+  mkdir -p $HOME/.spinnaker
+fi
 bork do ok symlink $HOME/.spinnaker/logback-defaults.xml $DIR/files/logback-defaults.xml
+if did_error; then
+  echo "Failed symlinking logging defaults"
+  exit 1
+fi
 
 # TODO rz - should be automatic
 echo "Add 'source $HOME/.spinnaker-env.sh' to your shell"
